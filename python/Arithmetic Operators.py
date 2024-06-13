2a08cd17cf35404a1a8ce68025ad0dbcca0cfd12
Lenmbugua <92791059+Lenmbugua@users.noreply.github.com>
2024-06-13 16:40:59 +0300

    Division

---
+++ python/Arithmetic Operators.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+        # Read two integers from STDIN
+        a = int(input())
+        b = int(input())
+
+        # Compute the sum of the two numbers
+        sum_ab = a + b
+
+        # Compute the difference of the two numbers
+        diff_ab = a - b
+
+        # Compute the product of the two numbers
+        prod_ab = a * b
+
+        # Print the results
+        print(sum_ab)
+        print(diff_ab)
+        print(prod_ab)
---
+++ python/Division.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    # Read two integers from STDIN
+    a = int(input())
+    b = int(input())
+    # Compute the Division of the two numbers
+    Division_ab = a // b
+    # Compute the float integer of the two numbers
+    floor_division_ab = a / b
+    # Print the results
+    print(Division_ab)
+    print(floor_division_ab)
